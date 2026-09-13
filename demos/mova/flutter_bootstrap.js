e9d343ce498cff795721b5b1563b83e98ec54d21
Mariam Portfolio <mariam-portfolio@users.noreply.github.com>
2026-09-13 09:58:58 +0300

    Fix demo language switching

--- demos/mova/flutter_bootstrap.js
+++ demos/mova/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3338302563" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3186266546" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
